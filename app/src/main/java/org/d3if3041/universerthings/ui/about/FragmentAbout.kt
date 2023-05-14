45f78642ef70394ae677a707b995a9ae12aa6fa7
Wulan <maulidyawulan@student.telkomuniversity.ac.id>
2023-05-14 13:13:37 +0700

    membuat halaman about (nofix)

---
+++ app/src/main/java/org/d3if3041/universerthings/ui/about/FragmentAbout.kt
@@ -0,0 +1,21 @@
+package org.d3if3041.universerthings.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.d3if3041.universerthings.databinding.FragmentAboutBinding
+
+class FragmentAbout : Fragment() {
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
