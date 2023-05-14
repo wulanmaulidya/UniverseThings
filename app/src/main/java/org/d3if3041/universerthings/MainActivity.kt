1e104b60ca9f868fdf2913987a29ed2427da1102
Wulan <maulidyawulan@student.telkomuniversity.ac.id>
2023-05-14 13:13:55 +0700

    implementasi navigasi & navUp

--- app/src/main/java/org/d3if3041/universerthings/MainActivity.kt
+++ app/src/main/java/org/d3if3041/universerthings/MainActivity.kt
@@ -3,11 +3,24 @@ package org.d3if3041.universerthings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 
         // membuat dialog exit
