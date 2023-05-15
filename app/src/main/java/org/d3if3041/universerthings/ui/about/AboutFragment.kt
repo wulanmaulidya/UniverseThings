f4156c2afd178d6cecaeef5c2157626686613034
Wulan <maulidyawulan@student.telkomuniversity.ac.id>
2023-05-15 13:07:15 +0700

    universethings fixed

--- app/src/main/java/org/d3if3041/universerthings/ui/about/AboutFragment.kt
+++ app/src/main/java/org/d3if3041/universerthings/ui/about/AboutFragment.kt
@@ -1,10 +1,12 @@
 package org.d3if3041.universerthings.ui.about
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.d3if3041.universerthings.R
 import org.d3if3041.universerthings.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -17,5 +19,17 @@ class AboutFragment : Fragment() {
     ): View? {
         binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.buttonBagikan.setOnClickListener { bagikanapp() }
+    }
+
+    fun bagikanapp(){
+        val sharingIntent = Intent(Intent.ACTION_SEND)
+        sharingIntent.type = "text/plain"
+        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, requireActivity().getString(R.string.app_name))
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, "Kumpulkan informasi tentang Tata Surya lebih lengkap dengan UniverseThings Sekarang!!!")
+        requireActivity().startActivity(Intent.createChooser(sharingIntent, "Bagikan Melalui"))
     }
 }
